0a1d68bd1eb0bfac692629f85887c27024f55928
Octavector <hello@octavector.co.uk>
2015-01-31 11:54:13 +0000

    Added keyboard control for player(1), W for up, S for down.

--- pong.js
+++ pong.js
@@ -4,10 +4,6 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d"); 
 
-    //Dummy variables for use in demo pendulum animation
-    var x = 0; 
-    var y = 0;
-
     //Game loop. repeatOften function is called continuously with requestAnimationFrame
     function repeatOften(){ 
         movePaddle();
@@ -17,12 +13,11 @@ window.onload = function(){
     //Begin game loop
     requestAnimationFrame(repeatOften);
 
+    //Create player object to hold player properties
+    var player = {x:40, y:20, up:false, down:false};
+
 
     function movePaddle(){
-        //Temporary, demo pendulum animation. X controls speed.
-        x += 0.02;
-        y = 170 +( 150*(Math.sin(x)));
-
         //Clear canvas ready for drawing new frame
         context.clearRect(0, 0, 700, 400);
         context.fillStyle="#000";
@@ -31,9 +26,37 @@ window.onload = function(){
         //Draw player paddle
         context.beginPath();
         context.fillStyle="white";
-        context.fillRect(40,y,10,100);
+        context.fillRect(40,player.y,10,100);
         context.stroke(); 
+
+        //Move player as per keys pressed, prevent from leaving game area
+        if(player.up && player.y > 20){player.y -= 3;}
+        if(player.down && player.y < 330){player.y += 3;}
     } 
-    
+
+    //Player Controls
+    //=======================================================================
+
+    //keyboard key event detection
+    window.addEventListener("keydown",keyControl,false);
+    window.addEventListener("keyup",clearKeyControl,false);
+
+    function keyControl(e){
+        player[getKey(e.which)] = true;
+    }
+
+    //On keyup, clear all "up" and "down" properties
+    function clearKeyControl(e){
+        player[getKey(e.which)] = false;
+    }
+
+    //W = up, S = down
+    function getKey(key){
+        switch(key){
+            case 87: return "up"; break;
+            case 83: return "down"; break;
+        }
+    }
+
 };
 
