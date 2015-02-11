df5282835d403d573942eef97a2acef37de63785
xismic <xismic@gmail.com>
2015-02-11 13:47:20 +0000

    Canvas split, dashed line and basic ball
    board (static) and game (dynamic)
    dashed line drawn down centre of game
    basic ball is spawned and moves in direction of initial variables
    
    TODO: ball collision detection

--- pong.js
+++ pong.js
@@ -1,16 +1,54 @@
 window.onload = function(){ 
 
-    //Fetch canvas and get 2d context for drawing
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
+    CanvasRenderingContext2D.prototype.dashedLine = function (x1, y1, x2, y2, dashLen) {
+        if (dashLen == undefined) dashLen = 2;
+        this.moveTo(x1, y1);
 
-    var gameSize = { width: 700, height: 400};
-    var paddleSize = { width: 10, height: 100};
-    var padding = { horizontal: 40, vertical: 20};
+        var dX = x2 - x1;
+        var dY = y2 - y1;
+        var dashes = Math.floor(Math.sqrt(dX * dX + dY * dY) / dashLen);
+        var dashX = dX / dashes;
+        var dashY = dY / dashes;
+
+        var q = 0;
+        while (q++ < dashes) {
+            x1 += dashX;
+            y1 += dashY;
+            this[q % 2 == 0 ? 'moveTo' : 'lineTo'](x1, y1);
+        }
+        this[q % 2 == 0 ? 'moveTo' : 'lineTo'](x2, y2);
+    };
+
+
+
+    //Fetch canvas and get 2d context for drawing
+    var board = document.getElementById("board");
+    var game = document.getElementById("game");
+
+    var boardContext = board.getContext("2d");
+    var gameContext = game.getContext("2d");
+
+    var gameSize = { width: 700, height: 450 };
+    var paddleSize = { width: 10, height: 100 };
+    var ballSize = { width: 10, height: 10 }
+    var padding = { horizontal: 40, vertical: 20 };
+
+
+    // Create Board
+    boardContext.clearRect(0, 0, gameSize.width, gameSize.height);
+    boardContext.fillStyle="#000";
+    boardContext.fillRect(0, 0, gameSize.width, gameSize.height);
+
+    boardContext.strokeStyle="#FFF";
+    boardContext.lineWidth = 5;
+    boardContext.dashedLine(gameSize.width/2, 0, gameSize.width/2, gameSize.height, gameSize.height/41);
+    boardContext.stroke();
 
     //Game loop. repeatOften function is called continuously with requestAnimationFrame
-    function repeatOften(){ 
+    function repeatOften(){
+        prepareDrawing();
         movePaddles();
+        moveBall();
         requestAnimationFrame(repeatOften);
     }
 
@@ -31,30 +69,45 @@ window.onload = function(){
         down:false
     };
 
+    var ball = {
+        x: gameSize.width/2,
+        y: gameSize.height/2,
+        angle: Math.PI/2,
+        velocity: 5
+    }
+
+    function prepareDrawing(){
+        gameContext.clearRect(0, 0, gameSize.width, gameSize.height);
+        gameContext.beginPath();
+    }
 
     function movePaddles(){
-        //Clear canvas ready for drawing new frame
-        context.clearRect(0, 0, gameSize.width, gameSize.height);
-        context.fillStyle="#000";
-        context.fillRect(0,0,canvas.width,canvas.height);
-
         // Draw player paddles
-        context.beginPath();
-        context.fillStyle="white";
-        context.fillRect(playerA.x,playerA.y,paddleSize.width,paddleSize.height);
-        context.fillRect(playerB.x,playerB.y,paddleSize.width,paddleSize.height);
+        gameContext.fillStyle="white";
+        gameContext.fillRect(playerA.x, playerA.y, paddleSize.width, paddleSize.height);
+        gameContext.fillRect(playerB.x, playerB.y, paddleSize.width, paddleSize.height);
 
         //Move player as per keys pressed, prevent from leaving game area
         if(playerA.up && playerA.y > padding.vertical)
-            playerA.y -= 3;
-        else if(playerA.down && playerA.y < gameSize.height-padding.vertical-(paddleSize.height/2))
-            playerA.y += 3;
+            playerA.y -= 5;
+        else if(playerA.down && playerA.y < gameSize.height-padding.vertical-paddleSize.height)
+            playerA.y += 5;
 
         if(playerB.up && playerB.y > padding.vertical)
-            playerB.y -= 3;
-        else if(playerB.down && playerB.y < gameSize.height-padding.vertical-(paddleSize.height/2))
-            playerB.y += 3;
+            playerB.y -= 5;
+        else if(playerB.down && playerB.y < gameSize.height-padding.vertical-paddleSize.height)
+            playerB.y += 5;
     } 
+
+    function moveBall(){
+        // Draw player paddles
+        gameContext.fillStyle="white";
+        gameContext.fillRect(ball.x, ball.y, ballSize.width, ballSize.height);
+
+        ball.x += ball.velocity * Math.sin(ball.angle);
+        ball.y += ball.velocity * Math.cos(ball.angle);
+    }
+
 
     //Player Controls
     //=======================================================================
